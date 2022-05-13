0d924193414342a3c92e17532d64df4e7f928e3b
FelixvL <digitaleservice@gmail.com>
2022-05-13 13:57:30 +0200

    GJ

--- Demo.java
+++ Demo.java
@@ -1,13 +1,13 @@
-// GI
+// GJ
 
 public class Demo {
 	public static void main(String[] args) {
-		Testje t = () -> "iets";
+		Testje t = (1, 2, 3) -> "anders";
 		String ab = t.gaan();
 		System.out.println(ab);
 	}
 }
 @FunctionalInterface 
 interface Testje{
-	String gaan();
+	String gaan(int a, int b, int c);
 }
