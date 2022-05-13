cd48ca8e84eeb0b062da4ec66daa9c86aeda472b
FelixvL <digitaleservice@gmail.com>
2022-05-13 14:02:56 +0200

    GL

--- Demo.java
+++ Demo.java
@@ -1,13 +1,13 @@
-// GK
+// GL
 
 public class Demo {
 	public static void main(String[] args) {
-		Testje t = (a, b, c) -> ""+ a + c;
-		String ab = t.gaan(1, true, 3);
+		Testje t = (a, c) -> a.length() > 5 && c;
+		String ab = t.gaan("true",true);
 		System.out.println(ab);
 	}
 }
 @FunctionalInterface 
 interface Testje{
-	String gaan(int a, boolean b, int c);
+	String gaan(String a, boolean b);
 }
