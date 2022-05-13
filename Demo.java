6b0c2cd153ff41469c3ded95cd4763d9efd9566d
FelixvL <digitaleservice@gmail.com>
2022-05-13 13:52:41 +0200

    GH

--- Demo.java
+++ Demo.java
@@ -1,6 +1,24 @@
-class Demo{
-	public static void main(String args[]){
-		System.out.println("starten");
+// GH
+
+public class Demo {
+	public static void main(String[] args) {
+	//	(  int getal1, int getal2  ) ->{ return int; }
+		Testje eersteLambda;
+		eersteLambda = (a)-> a.equals("hoi") ;
+		//eersteLambda = (a, b)-> a.length() > 5 && b ;
+		boolean tt = eersteLambda.DeAbstracteMethode("hoidaar");
+		System.out.println(tt);		
+		String ah = "goedeavond"+9+6;
+		boolean b = 8 > 6;
+		boolean c = true;
+//		Testje tweedeLambda;
+//		tweedeLambda = (q,w)-> q+q-w*3;
+//		int ww = tweedeLambda.DeAbstracteMethode(12, 15);
+//		System.out.println(ww);
 	}
-	
+}
+@FunctionalInterface       // 1 abstracte methode
+interface Testje{
+	// returntype               // aantal type paramters
+	boolean DeAbstracteMethode(  String getal1  );
 }
