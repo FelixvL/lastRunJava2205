34666962421824771dd02308cb59be3a488ab205
FelixvL <digitaleservice@gmail.com>
2022-05-13 14:31:19 +0200

    GN

--- Demo.java
+++ Demo.java
@@ -1,13 +1,32 @@
-// GM
+// GN
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
 
 public class Demo {
 	public static void main(String[] args) {
-		Testje t = (a, c) -> a.length() > 5 && c;
-		boolean ab = t.gaan("true",true);
-		System.out.println(ab);
+		//LocalTime lt = new LocalTime();
+		LocalDateTime ldt = LocalDateTime.now() ;
+		LocalDateTime tweede = ldt.minusDays(7);
+		System.out.println(ldt);
+		System.out.println(tweede);
+		
+		LocalDateTime derde = LocalDateTime.of(2021,11,3,4,5,6,12);
+		System.out.println(derde);
+		LocalDateTime vierde = tweede.minus(Period.of(2, 5, 3));
+		System.out.println(vierde);
+		System.out.println(vierde.getDayOfMonth());
+		System.out.println(vierde.getDayOfWeek());
+		System.out.println("Het was "+vierde.getDayOfWeek() +" "+vierde.getDayOfMonth()+ " "+vierde.getMonth() +" "+ vierde.getYear());
 	}
 }
-@FunctionalInterface 
-interface Testje{
-	boolean gaan(String a, boolean b);
-}
+// String StringBuilder
+// Wrappers ArrayList
+//LocalDate
+//LocalDateTime
+//Period
+//LocalTime
+//Duration
+
+
