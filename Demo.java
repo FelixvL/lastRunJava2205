91a6b633946d8f87477827c66a015539f893ebcb
FelixvL <digitaleservice@gmail.com>
2022-05-13 13:54:30 +0200

    GI

--- Demo.java
+++ Demo.java
@@ -1,24 +1,13 @@
-// GH
+// GI
 
 public class Demo {
 	public static void main(String[] args) {
-	//	(  int getal1, int getal2  ) ->{ return int; }
-		Testje eersteLambda;
-		eersteLambda = (a)-> a.equals("hoi") ;
-		//eersteLambda = (a, b)-> a.length() > 5 && b ;
-		boolean tt = eersteLambda.DeAbstracteMethode("hoidaar");
-		System.out.println(tt);		
-		String ah = "goedeavond"+9+6;
-		boolean b = 8 > 6;
-		boolean c = true;
-//		Testje tweedeLambda;
-//		tweedeLambda = (q,w)-> q+q-w*3;
-//		int ww = tweedeLambda.DeAbstracteMethode(12, 15);
-//		System.out.println(ww);
+		Testje t = () -> "iets";
+		String ab = t.gaan();
+		System.out.println(ab);
 	}
 }
-@FunctionalInterface       // 1 abstracte methode
+@FunctionalInterface 
 interface Testje{
-	// returntype               // aantal type paramters
-	boolean DeAbstracteMethode(  String getal1  );
+	String gaan();
 }
